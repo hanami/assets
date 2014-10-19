a3923f2cd6401aaf3694dd18c0ec50e2d6a102c1
Benny <r3qnbenni@gmail.com>
2014-10-19 11:14:37 +0200

    css_engine & js_engine should be configurable by the user

--- lib/lotus/assets.rb
+++ lib/lotus/assets.rb
@@ -1,10 +1,14 @@
 require 'lotus/assets/version'
 require 'lotus/assets/assets_helpers'
 
+require 'lotus/utils/class_attribute'
+
 module Lotus
   module Assets
+    include Utils::ClassAttribute
+
     class << self
-      attr_accessor :path
+      attr_accessor :path, :css_engine, :js_engine
     end
   end
 end
--- lib/lotus/assets/assets_helpers.rb
+++ lib/lotus/assets/assets_helpers.rb
@@ -10,16 +10,17 @@ module Lotus
 
         # TODO: find a better way to access the current Lotus::Config::Assets object
         Assets.path = Kernel.const_get(base_namespace)::Application.configuration.assets.to_s
+
+        base.extend(ClassMethods)
       end
 
       def stylesheet_include_tag
-        engine = 'scss'
         file   = 'application'
         base_path = "#{Assets.path}/stylesheets"
 
         # TODO: Implement caching system (maybe via mtime timestamp?)
         unless File.exist?("#{base_path}/#{file}.css")
-          main_template = Tilt.new("#{base_path}/#{file}.#{engine}")
+          main_template = Tilt.new("#{base_path}/#{file}.#{Assets.css_engine}")
 
           main_file = File.new("#{base_path}/#{file}.css", 'w')
           main_file.puts main_template.render
@@ -31,13 +32,12 @@ module Lotus
       end
 
       def javascript_include_tag
-        engine = 'coffee'
         file   = 'application'
         base_path = "#{Assets.path}/javascripts"
 
         # TODO: Implement caching system (maybe via mtime timestamp?)
         unless File.exist?("#{base_path}/#{file}.js")
-          main_template = Tilt.new("#{base_path}/#{file}.#{engine}")
+          main_template = Tilt.new("#{base_path}/#{file}.#{Assets.js_engine}")
 
           main_file = File.new("#{base_path}/#{file}.js", 'w')
           main_file.puts main_template.render
@@ -46,6 +46,24 @@ module Lotus
 
         # TODO: How to get proper base path? eg.: for lotus application mounted under /admin etc..
         "<script src='/admin/javascripts/#{file}.js'></script>"
+      end
+
+      module ClassMethods
+        def css_engine(engine = nil)
+          if engine
+            Assets.css_engine = engine
+          else
+            Assets.css_engine
+          end
+        end
+
+        def js_engine(engine = nil)
+          if engine
+            Assets.js_engine = engine
+          else
+            Assets.js_engine
+          end
+        end
       end
     end
   end

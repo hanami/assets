739d6adfe6622c5c1f6ae157030f93bef237e20d
Benny Klotz <r3qnbenni@gmail.com>
2014-10-23 00:10:44 +0200

    added test for including Lotus::Assets which in turn includes Lotus::Assets::Helpers via included hook

--- test/assets_test.rb
+++ test/assets_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
+class FixtureClass
+  include Lotus::Assets
+end
+
 describe Lotus::Assets do
+  describe 'including Lotus::Assets' do
+    it 'includes Lotus::Assets::Helpers into the base class' do
+      FixtureClass.ancestors.must_include Lotus::Assets::Helpers
+    end
+  end
 end

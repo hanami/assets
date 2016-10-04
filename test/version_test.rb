dfeb2d45065c7f50d0f3a4197fbfd1f54a1c8854
Luca Guidi <me@lucaguidi.com>
2016-10-04 18:18:47 +0200

    Bump to v0.4.0

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Hanami::Assets::VERSION do
   it 'exposes version' do
-    Hanami::Assets::VERSION.must_equal '0.3.0'
+    Hanami::Assets::VERSION.must_equal '0.4.0'
   end
 end

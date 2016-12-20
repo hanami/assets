6f56a7893d36f0a8cf730569dfadc5664d9ead2f
Luca Guidi <me@lucaguidi.com>
2016-12-20 13:07:13 +0100

    Bump version to 1.0.0.beta1

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Hanami::Assets::VERSION do
   it 'exposes version' do
-    Hanami::Assets::VERSION.must_equal '0.4.0'
+    Hanami::Assets::VERSION.must_equal '1.0.0.beta1'
   end
 end

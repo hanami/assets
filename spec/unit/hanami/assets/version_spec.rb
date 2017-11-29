5ef328d24d833e035a766ed5a61f0cf6b6cf52f3
Luca Guidi <me@lucaguidi.com>
2017-11-29 18:05:55 +0100

    Bump version to v2.0.0.alpha1

--- spec/unit/hanami/assets/version_spec.rb
+++ spec/unit/hanami/assets/version_spec.rb
@@ -2,6 +2,6 @@
 
 describe "Hanami::Assets::VERSION" do
   it "exposes version" do
-    expect(Hanami::Assets::VERSION).to eq("1.1.0")
+    expect(Hanami::Assets::VERSION).to eq("2.0.0.alpha1")
   end
 end

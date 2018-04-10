288f645fe7e23d6c0961f0f9b7e328d596308311
Luca Guidi <me@lucaguidi.com>
2018-04-10 10:52:48 +0200

    Prepare for v1.2.0

--- spec/unit/hanami/assets/version_spec.rb
+++ spec/unit/hanami/assets/version_spec.rb
@@ -1,5 +1,5 @@
 describe "Hanami::Assets::VERSION" do
   it "exposes version" do
-    expect(Hanami::Assets::VERSION).to eq("1.2.0.rc2")
+    expect(Hanami::Assets::VERSION).to eq("1.2.0")
   end
 end

0ced45df0984aa4815250cdef34c4b0808d8fe33
Tim Riley <tim@riley.id.au>
2023-09-18 23:34:57 +1000

    Expect manifest at all times, raise errors if not
    As our work on assets has evolved, we’ve determined that a manifest is required at all times.
    
    To ensure manifest-related errors can be directly addressed, eagerly return some errors when config is not pointing to a valid manifest file:
    
    - Raise a Hanami::Assets::ConfigError when the manifest_path is nil
    - Raise a Hanami::Assets::ManifestMissingError when the manifest_path is configured, but no file exists at that location
    
    Lastly, update the `Assets#[]` implementation to fetch sources from the manifest at all times.

--- lib/hanami/assets/errors.rb
+++ lib/hanami/assets/errors.rb
@@ -9,5 +9,26 @@ module Hanami
     # @since 0.1.0
     class Error < ::StandardError
     end
+
+    # Error raised when assets config is not valid.
+    #
+    # @since 2.1.0
+    # @api public
+    class ConfigError < Error
+    end
+
+    # Error returned when the assets manifest file is missing.
+    #
+    # @since 2.1.0
+    # @api public
+    class ManifestMissingError < Error
+      def initialize(manifest_path)
+        super(<<~TEXT)
+          Missing manifest file at #{manifest_path}.
+
+          Have you run `hanami assets compile` or `hanami assets watch`?
+        TEXT
+      end
+    end
   end
 end
